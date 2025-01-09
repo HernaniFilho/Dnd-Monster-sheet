d6eef46e70cf30ab4417a15a85fdc96ba2cae7b5
HernaniFilho <hernanipcf@gmail.com>
2025-01-09 10:52:45 -0300

    MySQL e script para criação de banco de dados

---
+++ resources/Script.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE monstersheet;
+
+CREATE TABLE Monster (
+    id int NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    alignment VARCHAR(10),
+    armorClass INTEGER,
+    hitPoints INTEGER NOT NULL,
+    speed INTEGER,
+    challenge INTEGER NOT NULL,
+    strength INTEGER NOT NULL,
+    dexterity INTEGER NOT NULL,
+    constitution INTEGER NOT NULL,
+    intelligence INTEGER NOT NULL,
+    wisdom INTEGER NOT NULL,
+    charisma INTEGER NOT NULL,
+    proficiencyBonus INTEGER NOT NULL
+)
